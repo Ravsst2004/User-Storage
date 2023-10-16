ed4743bb0a0f347e387ec7f3cc3bb6cb5e7c4289
Putu Ravindra Viswanatha <128573488+Ravsst2004@users.noreply.github.com>
2023-10-16 19:17:55 +0800

    #

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mt-5">Welcome to CRUD website, 😂😂</h1>
+<h1 class="mt-5">Welcome to User<span class="text-primary">Storage</span>, where you can store user data or just simply
+    call CRUD (create read update delete), everyone can use this web for nothing. What the hell am i write, i just write what im thinking</h1>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 
-<a href="/user/create" class="btn btn-primary text-decoration-none">ADD</a>
+<a href="/user/create" class="btn btn-primary text-decoration-none" style="width: 100px">ADD</a>
 
 
 @if (session()->has('success'))
@@ -12,41 +12,41 @@
 </div>
 @endif
 
-<table class="table mt-1">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Username</th>
-            <th scope="col">Email</th>
-            <th scope="col">Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @if ($users->count())
-        @foreach ($users as $user)
-        <tr>
-            <th scope="row">{{ $users->firstItem() + $loop->index }}</th>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->username }}</td>
-            <td>{{ $user->email }}</td>
-            <td class="d-flex">
-                <a href="user/{{ $user->id }}/edit" class="btn btn-warning text-decoration-none">Edit</a>
-                <form action="user/{{ $user->id }}" method="POST" id="delete-form">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit" class="btn btn-danger mx-1">DELETE</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-        @else
-            <h1>User Not Found!</h1>
-        @endif
-    </tbody>
-
-
-</table>
+<div class="container border border-secondary-subtle my-3 ">
+    <table class="table mt-1">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Username</th>
+                <th scope="col">Email</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if ($users->count())
+            @foreach ($users as $user)
+            <tr>
+                <th scope="row">{{ $users->firstItem() + $loop->index }}</th>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->email }}</td>
+                <td class="d-flex">
+                    <a href="user/{{ $user->id }}/edit" class="btn btn-warning text-decoration-none" >Edit</a>
+                    <form action="user/{{ $user->id }}" method="POST" id="delete-form" onsubmit="return confirm('Are you sure?')">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-danger mx-1" >DELETE</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+            @else
+                <h1>User Not Found!</h1>
+            @endif
+        </tbody>
+    </table>
+</div>
 
 {{ $users->links() }}
 @endsection
