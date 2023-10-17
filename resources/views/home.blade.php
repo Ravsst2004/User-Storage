39e9e7e329881d2cfecab759889abd9cc4a38c9a
Putu Ravindra Viswanatha <128573488+Ravsst2004@users.noreply.github.com>
2023-10-17 10:03:43 +0800

    #

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mt-5">Welcome to User<span class="text-primary">Storage</span>, where you can store user data or just simply
-    call CRUD (create read update delete), everyone can use this web for nothing. What the hell am i write, i just write what im thinking</h1>
+{{-- Content welcome --}}
+<div class="my-content container mt-5">
+    <div class="my-welcome row">
+        <div class="col-lg-12">
+            <h1 class="fw-bold">Welcome to User<span class="text-primary">Storage</span> </h1>
+            <p class="fw-medium fs-4 mt-3">"UserStorage is a simple CRUD (Create Read Update Delete) website" - Thomas Slebew</p>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,41 +12,42 @@
 </div>
 @endif
 
-<div class="container border border-secondary-subtle my-3 ">
-    <table class="table mt-1">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Username</th>
-                <th scope="col">Email</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @if ($users->count())
-            @foreach ($users as $user)
-            <tr>
-                <th scope="row">{{ $users->firstItem() + $loop->index }}</th>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->email }}</td>
-                <td class="d-flex">
-                    <a href="user/{{ $user->id }}/edit" class="btn btn-warning text-decoration-none" >Edit</a>
-                    <form action="user/{{ $user->id }}" method="POST" id="delete-form" onsubmit="return confirm('Are you sure?')">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit" class="btn btn-danger mx-1" >DELETE</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-            @else
-                <h1>User Not Found!</h1>
-            @endif
-        </tbody>
-    </table>
-</div>
+
+<table class="table mt-1">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+            <th scope="col">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @if ($users->count())
+        @foreach ($users as $user)
+        <tr>
+            <th scope="row">{{ $users->firstItem() + $loop->index }}</th>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->username }}</td>
+            <td>{{ $user->email }}</td>
+            <td class="d-flex">
+                <a href="user/{{ $user->id }}/edit" class="btn btn-warning text-decoration-none">Edit</a>
+                <form action="user/{{ $user->id }}" method="POST" id="delete-form"
+                    onsubmit="return confirm('Are you sure?')">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-danger mx-1">DELETE</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+        @else
+        <h1>User Not Found!</h1>
+        @endif
+    </tbody>
+</table>
+
 
 {{ $users->links() }}
 @endsection
